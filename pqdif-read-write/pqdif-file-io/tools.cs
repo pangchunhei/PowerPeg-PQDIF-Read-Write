236f16cc2949bcb7c8a1975411aaafc804253efe
Elston Pang <elstonpang2000@gmail.com>
2022-10-06 03:18:45 +0800

    Test code with GEMSTONE library

---
+++ pqdif-read-write/pqdif-file-io/tools.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Gemstone.PQDIF.Logical;
+//using Gemstone.PQDIF.Logical;
+using Gemstone.PQDIF.Physical;
+
+namespace pqdif_file_io
+{
+    public class tools
+    {
+        /*public static void read(string fileName)
+        {
+            LogicalParser parser;
+
+            parser = new LogicalParser(fileName);
+            parser.OpenAsync();
+
+            Console.WriteLine(parser.ContainerRecord.PhysicalRecord);
+            Console.WriteLine(parser.ContainerRecord.PhysicalRecord.Body.Collection);
+
+            //Task<Record> record;
+            //record = parser.GetNextRecordAsync();
+            //Console.WriteLine(record.ToString()); 
+            //Console.WriteLine();
+
+            Console.ReadLine();
+        }*/
+
+        public static void read(string filePath)
+        {
+            using PhysicalParser parser = new PhysicalParser(filePath);
+            parser.OpenAsync();
+
+            while (parser.HasNextRecord())
+            {
+                Task<Record> record = parser.GetNextRecordAsync();
+                Console.WriteLine(record.Result);
+                Console.WriteLine();
+            }
+        }
+
+        public static void defaultLog(string filePath)
+        {
+            ContainerRecord containerRecord;
+            List<ObservationRecord> observationRecords = new List<ObservationRecord>();
+
+            using LogicalParser parser = new LogicalParser(filePath);
+            parser.OpenAsync();
+            containerRecord = parser.ContainerRecord;
+            while (parser.HasNextObservationRecordAsync())
+            {
+                observationRecords.Add(parser.NextObservationRecordAsync());
+            }
+        }
+}
+}
---
+++ pqdif-read-write/test/Program.cs
@@ -0,0 +1,7 @@
+﻿using pqdif_file_io;
+
+// See https://aka.ms/new-console-template for more information
+
+tools.read("C:\\Users\\elsto\\Documents\\GitHub\\PowerPeg-PQDIF-Read-Write\\pqdif-read-write\\test\\Test\\t1.pqd");
+
+Console.WriteLine("Hello, World!");
