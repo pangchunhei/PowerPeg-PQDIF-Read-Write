d6bc582a9ff42256c3c5bdc82ceca7b47df1c3ae
Elston Pang <elstonpang2000@gmail.com>
2022-10-07 02:08:26 +0800

    Load original data

--- pqdif-read-write/pqdif-file-io/tools.cs
+++ pqdif-read-write/pqdif-file-io/tools.cs
@@ -20,10 +20,28 @@ namespace pqdif_file_io
                 observationRecords.Add(await parser.NextObservationRecordAsync());
             }
 
-            foreach(var v in observationRecords)
+            foreach (ObservationRecord observationRecord in observationRecords)
             {
-                Console.WriteLine(v.PhysicalRecord.Body);
+                Console.Write(observationRecord.StartTime);
+
+                // Get the first channel instance
+                ChannelInstance firstChannelInstance = observationRecord.ChannelInstances[0];
+
+
+                foreach (SeriesInstance seriesInstance in firstChannelInstance.SeriesInstances)
+                {
+                    // FYI, this expands StorageMethods.Increment and applies
+                    // scale/offset and transducer ratio fields to the values,
+                    // as opposed to seriesInstance.SeriesValues which returns
+                    // the unmodified raw values as a VectorElement
+                    IList<object> values = seriesInstance.OriginalValues;
+                    string csv1 = string.Join(",", values);
+                    Console.WriteLine(csv1);
+                }
+
+                Console.WriteLine();
             }
+
         }
     }
 }
